91885375755f5d4d882ab474e9da8d5877710ae6
AppleNote <a7201492425@gmail.com>
2026-05-15 10:32:01 +0900

    수정

--- day 2/js/03rendermovie.js
+++ day 2/js/03rendermovie.js
@@ -3,7 +3,7 @@ const loadMovieBtn = document.querySelector("#loadMovieBtn");
 const movieList = document.querySelector("#movieList");
 const movieConsole = document.querySelector("#movieConsole");
 
-const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYWQ3ZDczMGI0N2Y4Nzg3NDg3OWQ1MzY0ZDEyZDlhOCIsIm5iZiI6MTY3NTIyMDMyNC44NjIwMDAyLCJzdWIiOiI2M2Q5ZDU2NGE5MTE3ZjAwNzk3NzI0ODIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.W0I3Zx3AjcW2p0mk_PNW1RUbzJny1MmburXPGmtgNjs";
+const TMDB_TOKEN = ""; // TMDB API 토큰을 입력합니다.
 const TMDB_URL = "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
